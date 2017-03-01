92f1f829a5517f325d3e7355bedcda5d4bb8b8ad
Doug Moscrop <doug.moscrop@gmail.com>
2017-03-01 14:03:33 -0500

    add new test case

--- __tests__/get-dependency-list.js
+++ __tests__/get-dependency-list.js
@@ -54,5 +54,14 @@ test('handles requiring dependency file in scoped package', (t) => {
 
 	const list = getDependencyList(fileName);
 
-	t.true(list.some(item => item.match(/@test\/scoped-dep/)));
+	t.true(list.some(item => item.indexOf(`@test/scoped-dep`) !== -1));
+});
+
+test('should handle requires with same relative path but different absolute path', (t) => {
+  const fileName = path.join(__dirname, 'fixtures', 'same-relative-require.js');
+
+  const list = getDependencyList(fileName);
+
+  t.true(list.some(item => item.indexOf(`bar/baz.js`) !== -1));
+  t.true(list.some(item => item.indexOf(`foo/baz.js`) !== -1));
 });
