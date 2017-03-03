c42d9a8f57f6f6b54c98a62052bcfc52e52531a3
Doug Moscrop <doug.moscrop@gmail.com>
2017-03-03 14:43:57 -0500

    excluding aws-sdk does not work this way.

--- get-dependency-list.js
+++ get-dependency-list.js
@@ -22,7 +22,7 @@ module.exports = function(filename, serverless) {
     }
 
     precinct.paperwork(current).forEach(name => {
-      if (resolve.isCore(name) || name === 'aws-sdk') {
+      if (resolve.isCore(name)) {
         return;
       }
 
@@ -63,10 +63,6 @@ module.exports = function(filename, serverless) {
 
     if (pkg.dependencies) {
       Object.keys(pkg.dependencies).forEach(dependency => {
-        if (dependency === 'aws-sdk') {
-          return;
-        }
-
         const pkg = resolvePkg(dependency, {
           cwd: current
         });
@@ -81,10 +77,6 @@ module.exports = function(filename, serverless) {
 
     if (pkg.optionalDependencies) {
       Object.keys(pkg.optionalDependencies).forEach(dependency => {
-        if (dependency === 'aws-sdk') {
-          return;
-        }
-
         const pkg = resolvePkg(dependency, {
           cwd: current
         });
